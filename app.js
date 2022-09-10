15b4008a25ed95f7319adc97de2d6b25faf660cf
andressa-leite <dre_tl@hotmail.com>
2022-09-10 15:01:15 -0300

    foi implementado um ROUTER  para entender como funciona

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ const app = express();
 
 const {infoCursos} = require('./cursos.js');
 
-//ROTAS: o .get abaixo é o método http e a barra('/') é o caminho 
+
+//ROUTERS -- criador de rota
+/*O routerProgramacion foi utilizado apenas uma vez (na linha 24) porque foi apenas para entender como o router funciona mas daria para fazer em todas as rotas*/
+const routerProgramacion = express.Router();
+app.use('/cursos/programacion', routerProgramacion);
+
+
+//ROUTING - ROTAS: o .get abaixo é o método http e a barra('/') é o caminho 
 app.get('/', (req, res) => {
     res.send('Mi primer servidor. Cursos ')
 });
@@ -14,17 +21,23 @@ app.get('/cursos', (req, res) => {
 });
 
 //PROGRAMACION
-app.get('/cursos/programacion', (req,res) => {
+routerProgramacion.get('/', (req,res) => {
     res.send(JSON.stringify(infoCursos.programacion));
 });
 
 // LINGUAGEM programacion
-//aqui o ":lenguaje" é um parametro url porque poderia haver centenas de linguagens cadastradas no curso de programaçao e isso faria com que eu precisasse cadastrar centenas de rotas 
+//aqui o ":lenguaje" é um PARAMETRO URL porque poderia haver centenas de linguagens cadastradas no curso de programaçao e isso faria com que eu precisasse cadastrar centenas de rotas 
 app.get('/cursos/programacion/:lenguaje', (req,res) => {
     const lenguaje = req.params.lenguaje;
     const resultados = infoCursos.programacion.filter(curso => curso.lenguaje === lenguaje);
         if (resultados.length === 0){
         return res.status(404).send(`No se encontraram cursos de ${lenguaje}`)
+        }
+    //PARAMETROS QUERY
+        if (req.query.ordenar === 'vistas') {
+            return res.send(JSON.stringify(resultados.sort((a,b) => b.vistas - a.vistas)));
+        } else {
+            res.send(JSON.stringify(resultados));
         }
     res.send(JSON.stringify(resultados))
 });
